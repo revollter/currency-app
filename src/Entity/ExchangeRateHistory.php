93ebeb4e78e801d1312661bcc0af481ec9616ddd
mszczachor <maciej.szczachor@sbg.com.pl>
2019-08-21 19:58:55 +0200

    refs #5 create entities

---
+++ src/Entity/ExchangeRateHistory.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ExchangeRateHistoryRepository")
+ */
+class ExchangeRateHistory
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $effectiveDate;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $rate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEffectiveDate(): ?\DateTimeInterface
+    {
+        return $this->effectiveDate;
+    }
+
+    public function setEffectiveDate(\DateTimeInterface $effectiveDate): self
+    {
+        $this->effectiveDate = $effectiveDate;
+
+        return $this;
+    }
+
+    public function getRate(): ?float
+    {
+        return $this->rate;
+    }
+
+    public function setRate(float $rate): self
+    {
+        $this->rate = $rate;
+
+        return $this;
+    }
+}
